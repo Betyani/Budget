eebb3844ce073272722f6ed4c127f61200a996d4
ohjonghun <dhwhdgns777@gmail.com>
2025-12-14 19:59:09 +0900

    수입지출 창 만들기 중

---
+++ src/main/java/com/controller/IncomeExpenseController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class IncomeExpenseController {
+
+    @FXML private Label dateLabel;
+
+    @FXML
+    private void initialize() {
+        // 지금은 화면 뜨는지 확인용
+        // dateLabel.setText("선택한 날짜: 테스트");
+    }
+}
